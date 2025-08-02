92e2807178b63a465c8ec63fb48eea01ac175c56
Md Muntasir Khan <195245704+mdmuntasirkhan@users.noreply.github.com>
2025-08-02 19:28:38 -0400

    Initial Update

---
+++ Source/FrontierSteward/FrontierSteward.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "FrontierSteward.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, FrontierSteward, "FrontierSteward" );
